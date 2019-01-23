63e25a84f0dccdf32b898e3cb3128d9c06725035
Thejesh P R <tp-r@microfocus.com>
2019-01-23 17:44:18 +0530

    created blog models

--- website/blogapp/models.py
+++ website/blogapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
-# Create your models here.
+from django.template.defaultfilters import slugify
+from django.utils.translation import gettext as _
+from taggit.managers import TaggableManager
+class BaseModel(models.Model):
+    """ Base Model for all Models """
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modfied_at = models.DateTimeField(auto_now=True)
+class Author(BaseModel):
+    """ Author Model """
+    email = models.EmailField(help_text=_("Email ID"))
+    name = models.TextField(max_length=200, help_text=_("Name of the Author"))
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return self.name
+    
