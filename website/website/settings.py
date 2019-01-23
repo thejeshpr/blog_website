08cff1150b5777b44c5f29c9bce8176ab2c2cde6
Thejesh P R <tp-r@microfocus.com>
2019-01-23 16:28:39 +0530

    updated timezone

--- website/website/settings.py
+++ website/website/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Kolkata'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
