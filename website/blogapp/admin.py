7e132099670180742d4b34a7ddd668104064f0e5
Thejesh P R <tp-r@microfocus.com>
2019-01-24 09:55:24 +0530

    user model updated

--- website/blogapp/admin.py
+++ website/blogapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article,  Profile, Category
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'active', 'created_at')
+    list_filter = ('active', 'created_at')
+    prepopulated_fields = {'slug': ('title', )}
+    search_fields = ('title', 'slug')
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'created_at')
+    list_filter = ('active', )
+    prepopulated_fields = {'slug': ('name', )}
+    search_fields = ('name', 'slug')    
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'active', 'created_at')
+    list_filter = ('active', )
+    # prepopulated_fields = {'slug': ('user', )}
+    search_fields = ('user', 'slug')    
+admin.site.register(Profile, ProfileAdmin)
--- website/blogapp/models.py
+++ website/blogapp/models.py
@@ -1,3 +1,6 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.utils.translation import gettext as _
@@ -9,30 +12,32 @@ class BaseModel(models.Model):
     active = models.BooleanField(default=True, help_text=_("Active?"))
     created_at = models.DateTimeField(auto_now_add=True)
     modfied_at = models.DateTimeField(auto_now=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
 
-class Author(BaseModel):
-    """ Author Model """
-    email = models.EmailField(help_text=_("Email ID"))
-    name = models.TextField(max_length=200, help_text=_("Name of the Author"))
-    slug = models.SlugField(unique=True)
+class Profile(BaseModel):
+    """ Profile Model """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(help_text="About Author")    
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    slug = models.SlugField(unique=True)    
 
     def __str__(self):
-        return self.name
+        return self.user.username
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Author, self).save(*args, **kwargs)
+        self.slug = slugify(self.user)
+        super(Profile, self).save(*args, **kwargs)
 
 
 class Category(BaseModel):
     """ Categories Model """
     created_by = models.ForeignKey(
-        Author,
+        User,
         on_delete=models.SET(1),
-        related_name="created_categories",
-        help_text=_("Created User Name"))
-    name = models.TextField(max_length=200, unique=True, help_text=_("Category Name"))
+        related_name="created_categories")
+    name = models.CharField(max_length=200, unique=True, help_text=_("Category Name"))
     slug = models.SlugField(unique=True)
 
     class Meta:        
@@ -52,11 +57,12 @@ class Article(BaseModel):
         Category, on_delete=models.SET(1),
         related_name="articles", help_text=_("Article Category"))    
     content = models.TextField(help_text=_("Content"))
-    created_by = models.ForeignKey(Author, on_delete=models.SET(1), related_name="articles")
+    owner = models.ForeignKey(User, on_delete=models.SET(1), related_name="articles")
     draft = models.BooleanField(default=True, help_text=_("Draft"))
+    img = models.URLField(blank=True, help_text="Article Image")
     published_date = models.DateTimeField(help_text=_("Article Published Date"))
     read_time = models.CharField(max_length=100, blank=True, null=True)
-    title = models.TextField(
+    title = models.CharField(
         max_length=300, unique_for_date=published_date,
         help_text="Article Title")
     slug = models.SlugField(unique_for_date=published_date)
